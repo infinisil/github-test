b067b84de4bac16142ece6574a4896d6073f7d9b
Anon <anon@anonymous.example>
2023-08-28 22:29:06 +0200

    bla

--- update.sh
+++ update.sh
@@ -8,10 +8,12 @@ pinFile=prebuilt-commit.json
 if [[ -e "$pinFile" ]]; then
   oldRev=$(jq -r '.rev' "$pinFile")
 
+  filesChanged=$(git diff "$oldRev" "$newRev" --name-only)
+  echo "Files changed: '$filesChanged'" >&2
   # If the difference between the old pin and the new pin is only the pinned file itself,
   # it means that a previous update of a pin itself caused the channel update.
   # We don't want to cause another channel update just because of the pin.
-  if [[ "$(git diff "$oldRev" "$newRev" --name-only)" == "$pinFile" ]]; then
+  if [[ "$filesChanged" == "$pinFile" ]]; then
     exit 1
   fi
 fi
