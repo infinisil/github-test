72ad1dab12d248f67db3d0bd291f66394d270a2d
Anon <anon@anonymous.example>
2023-08-28 22:26:30 +0200

    Udpate

---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+newRev=$1
+
+pinFile=prebuilt-commit.json
+
+if [[ -e "$pinFile" ]]; then
+  oldRev=$(jq '.rev' "$pinFile")
+
+  # If the difference between the old pin and the new pin is only the pinned file itself,
+  # it means that a previous update of a pin itself caused the channel update.
+  # We don't want to cause another channel update just because of the pin.
+  if [[ "$(git diff "$oldRev" "$newRev" --name-only)" == "$pinFile" ]]; then
+    exit 1
+  fi
+fi
+
+
+narHash=$(nix-prefetch-url \
+  "${GITHUB_SERVER_URL:-https://github.com}/${GITHUB_REPOSITORY:-NixOS/nixpkgs}/tarball/$newRev" \
+  --type sha256 --unpack)
+
+jq -n > "$pinFile" \
+  --arg rev "$newRev" \
+  --arg narHash "$narHash" \
+  '$ARGS.named'
